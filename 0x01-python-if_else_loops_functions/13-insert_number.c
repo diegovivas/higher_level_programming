8fc4de1d9e368909c9b22b9a1868edeeafc5a984
Diego Vivas <dieguitovivas@hotmail.com>
2019-09-10 23:42:03 +0000

    Feature 13 file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_node - check the code for Holberton School students.
+ * @head:structure doble pointer
+ * @number:index;
+ * Return: head.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *nodo;
+	listint_t *ojo;
+
+	nodo = malloc(sizeof(listint_t));
+	if (!nodo)
+	{
+		return (NULL);
+	}
+	nodo->n = number;
+	nodo->next = NULL;
+
+	ojo = *head;
+	while (ojo->next)
+	{
+		if (nodo->n >= ojo->next->n)
+			ojo = ojo->next;
+		else
+			break;
+	}
+	if (ojo->next == NULL)
+		return (NULL);
+
+	nodo->next = ojo->next;
+	ojo->next = nodo;
+	return (nodo);
+}
