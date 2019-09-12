aab7c276cc270c0521b70cded7a72bdf954159e6
diegovivas <dieguitovivas@hotmail.com>
2019-09-12 23:43:50 +0000

    Fix adding main conditional

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-import sys
-if len(sys.argv) == 2:
-    print('1 argument:')
-elif len(sys.argv) > 2:
-    print('{} arguments:'.format(len(sys.argv)-1))
-for x  in range(len(sys.argv)):
-    if len(sys.argv) == 1:
-        print('0 arguments.')
-    else:
-        if x > 0:
-            print ('{}: '.format(x), str(sys.argv[x]))
-            x = x + 1
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        print('1 argument:')
+    elif len(sys.argv) > 2:
+        print('{} arguments:'.format(len(sys.argv)-1))
+    for x  in range(len(sys.argv)):
+        if len(sys.argv) == 1:
+            print('0 arguments.')
+        else:
+            if x > 0:
+                print ('{}: '.format(x), str(sys.argv[x]))
+                x = x + 1
