4256f385b624ace7c916d9bcf367d96bb2bbb081
diegovivas <dieguitovivas@hotmail.com>
2019-09-12 23:55:31 +0000

    Fix space in output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    if len(sys.argv) == 1:
+        print('0 arguments.')
     if len(sys.argv) == 2:
         print('1 argument:')
-    elif len(sys.argv) > 2:
-        print('{} arguments:'.format(len(sys.argv)-1))
+    if len(sys.argv) > 2:
+        print('{} arguments:'.format(len(sys.argv) - 1))
     for x in range(len(sys.argv)):
-        if len(sys.argv) == 1:
-            print('0 arguments.')
-        else:
-            if x > 0:
-                print('{}: '.format(x), str(sys.argv[x]))
-                x = x + 1
+        if x > 0:
+            print('{}:'.format(x), str(sys.argv[x]))
+            x = x + 1
