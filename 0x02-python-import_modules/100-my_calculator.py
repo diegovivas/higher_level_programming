e97a8df0c0473f9d716330ef2ea5485d37ffa8ee
diegovivas <dieguitovivas@hotmail.com>
2019-09-13 03:57:12 +0000

    Fix more

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if sys.argv[2] == "+":
-        c = add(a, b)
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif sys.argv[2] == "-":
-        c = sub(a, b)
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif sys.argv[2] == "*":
-        c = mul(a, b)
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif sys.argv[2] == "/":
-        c = div(a, b)
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print(c)
+
