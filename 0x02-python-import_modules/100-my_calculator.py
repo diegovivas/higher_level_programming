c51a10254276d5f392217468e0ab592c95c1b697
diegovivas <dieguitovivas@hotmail.com>
2019-09-13 03:52:51 +0000

    Fix bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,14 +5,17 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == "+":
-        print(int(sys.argv[1]) + int(sys.argv[3]))
+        c = add(a, b)
     elif sys.argv[2] == "-":
-        print(int(sys.argv[1]) - int(sys.argv[3]))
+        c = sub(a, b)
     elif sys.argv[2] == "*":
-        print(int(sys.argv[1]) * int(sys.argv[3]))
+        c = mul(a, b)
     elif sys.argv[2] == "/":
-        print(int(sys.argv[1]) / int(sys.argv[3]))
+        c = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+    print(c)
