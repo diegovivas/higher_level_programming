d8581b6292385f18ce0c372fa41feb0c16e23013
Diego Vivas <dieguitovivas@hotmail.com>
2019-09-18 02:32:02 +0000

    Feature is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: 1 if is palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ojo, *ojo2;
+	int tama = 0;
+	int ciclador = 0;
+	int iterador = 0;
+	int otra = 0;
+
+	if (!*head)
+		return (0);
+	ojo = *head;
+	ojo2 = *head;
+	while (ojo->next)
+	{
+		ojo = ojo->next;
+		tama++;
+	}
+	ojo = *head;
+	ciclador = tama + 1;
+	if (tama % 2 == 0)
+		otra = (tama / 2) - 1;
+	else
+		otra = tama - 2;
+	while (iterador < ciclador && tama != otra)
+	{
+		ojo2 = ojo2->next;
+		iterador++;
+		if (iterador == tama)
+		{
+			if (ojo->n == ojo2->n)
+			{
+				iterador = 0;
+				tama--;
+				ojo2 = *head;
+				ojo = ojo->next;
+			}
+			else
+				return (0);
+		}
+	}
+	return (1);
+}
