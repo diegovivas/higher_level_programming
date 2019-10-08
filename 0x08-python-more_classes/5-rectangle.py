0f15ee03aa3073e6230f426e3f97870754631fb3
diegovivas <dieguitovivas@hotmail.com>
2019-10-08 04:32:46 +0000

    last features :( and fixes

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __str__(self):
+        if self.__width != 0 and self.__height != 0:
+            strin = "#" * self.__width + "\n"
+            strin = strin * self.__height
+            return strin[:-1]
+        else:
+            return ("")
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height != 0 and self.__width != 0:
+            return ((self.__height * 2) + (self.__width * 2))
+        else:
+            return 0
