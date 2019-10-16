a690172dbea4db9b291981acd813fa02ee21c48f
diegovivas <dieguitovivas@hotmail.com>
2019-10-16 18:32:08 +0000

    Feature: first files

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+    count = 0
+    with open(filename, mode='r', encoding="utf-8") as file: 
+        for line in file:
+            count += 1
+    return count
---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    count = 0
+    with open(filename, mode='r', encoding="utf-8") as file: 
+        if nb_lines <= 0:
+            print(file.read(), end="")
+        for line in file:
+            count += 1
+            if count <= nb_lines:
+                print(line, end="")
+    return count
