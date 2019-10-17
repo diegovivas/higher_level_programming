5d3a151bb5d1e8eaf4eb4c6c190eee589485f8bc
diegovivas <dieguitovivas@hotmail.com>
2019-10-17 00:11:54 +0000

    Feature: 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        diict = {}
+        hola = self.__dict__
+        if type(attrs) == list:
+            for at in attrs:
+                try:
+                    diict.setdefault(at, self.__dict__[at])
+                except Exception:
+                    pass
+        else:
+            return hola
+
+        return diict
