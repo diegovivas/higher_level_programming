236b3b453a2a765e3553c12d7e6f4071ba6d79af
diegovivas <dieguitovivas@hotmail.com>
2019-10-22 17:51:47 +0000

    comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,9 +4,12 @@
 import json
 
 class Base:
+"""Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """__init__ function"""
+
         if id is not None:
             self.id = id
         else:
@@ -17,6 +20,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """to_json_string function"""
+
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
@@ -24,4 +29,36 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        pass
+        """save_to_file function"""
+
+        filename = cls.__name__ + ".json"
+
+        lista = []
+        if list_objs:
+            for x in list_objs:
+                r = x.to_dictionary()
+                lista.append(r)
+        diccionario = cls.to_json_string(lista)
+        with open(filename, mode='w') as f:
+            f.write(diccionario )
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from_json_string"""
+
+        lista = []
+        if json_string:
+            return json.loads(json_string)
+        else:
+            return lista
+            
+    @classmethod
+    def create(cls, **dictionary):
+        """create function"""
+
+        if cls.__name__ == "Rectangle":
+            dummie = cls(1, 10)
+        elif cls.__name__ == "Square":
+            dummie = cls(2)
+        dummie.update(**dictionary)
+        return dummie
