b27efe78e65ffcb3c1177789fdf29213c3b84745
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 00:53:29 +0000

    method to filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                            charset="utf8")
     cur = conn.cursor()
     cur.execute('''
-    SELECT * FROM states WHERE name LIKE '%{}%' ORDER BY id ASC
+    SELECT * FROM states WHERE name = '{}' ORDER BY id ASC
     '''.format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
