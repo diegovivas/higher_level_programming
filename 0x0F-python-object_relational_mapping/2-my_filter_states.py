6119b5fa31196c0b76fdf0c78aa29c55ede4a99e
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 00:44:29 +0000

    query from user

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,10 +10,9 @@ if __name__ == "__main__":
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        if sys.argv[4] in row[1]:
             print(row)
     cur.close()
     conn.close()
