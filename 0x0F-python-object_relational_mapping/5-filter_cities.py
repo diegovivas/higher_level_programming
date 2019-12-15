a2109f6fc2c05f112fc4011d521a4e27ccf6f347
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 03:21:42 +0000

    filter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -28,15 +28,12 @@ if __name__ == "__main__":
     cur.execute(arg, argv)
     query_rows = cur.fetchall()
     x = len(query_rows)
-    flag = 0
 
     for row in query_rows:
-        flag = 1
         if row != query_rows[x - 1]:
             print(row[0], end=", ")
         else:
             print(row[0], end="")
-    if flag == 1:
-        print()
+    print()
     cur.close()
     conn.close()
