0afbaa53ed1adb9ac87953550bbbbae2cc21dd97
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 00:35:01 +0000

    pep8 fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     """Script that list database"""
     conn = MySQLdb.connect(host="localhost",
                            port=3306,
-                           user= sys.argv[1],
-                           passwd= sys.argv[2],
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,12 +2,19 @@
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user= sys.argv[1], passwd= sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    if "N" in row[1]:
-        print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    """filter whit N"""
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if "N" in row[1]:
+            print(row)
+    cur.close()
+    conn.close()
