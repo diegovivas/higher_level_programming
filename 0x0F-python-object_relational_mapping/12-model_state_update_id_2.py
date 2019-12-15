a2572a412bc5dfc11af9bf3e3e9fd63da573a41f
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 21:41:12 +0000

    Feature adding 12 task

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    """list all elements of State table"""
+    arg = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1],
+                                                           sys.argv[2],
+                                                           sys.argv[3])
+    engine = create_engine(arg, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+    for elements in states:
+        if elements.id == 2:
+            elements.name = 'New Mexico'
+            print(elements.name)
+    session.commit()
