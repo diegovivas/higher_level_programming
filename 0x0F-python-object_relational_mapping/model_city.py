cb5a255c5802558c016a678fb3a3e695c43d71c9
diegovivas <dieguitovivas@hotmail.com>
2019-12-15 22:07:49 +0000

    fix pep8 model_city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
-
 class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
