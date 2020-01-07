c3e704bc7b1e3f08ba666fab1983fda51fa37e35
diegovivas <dieguitovivas@hotmail.com>
2020-01-07 19:48:38 +0000

    [fix] task 0 manage args

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays the size of the body of the response
-curl -size 0.0.0.0:5000 | grep Content-Length:  |cut -d ' ' -f 2
+curl -size "$1" | grep Content-Length:  |cut -d ' ' -f 2
