f2b9225c5149adcfd8bb90d565d8884f3545b291
diegovivas <dieguitovivas@hotmail.com>
2020-01-13 22:35:15 +0000

    [feature] first task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
-hola 
+#!/usr/bin/python3
+""" fetches https://intranet.hbtn.io/status """
+
+import urllib.request
+
+if __name__=="__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as fetch:
+        page = fetch.read()
+        print("Body response:")
+        print("\t- type: " + str(type(page)))
+        print("\t- content: " + str(page))
+        print("\t- utf8 content: " + page.decode('utf-8'))
