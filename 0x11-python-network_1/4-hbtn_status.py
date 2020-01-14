2b4fda8a5c27141125f08d7572b8172478b97a32
diegovivas <dieguitovivas@hotmail.com>
2020-01-14 17:46:14 +0000

    [fix] task 4 requests library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ fetches https://intranet.hbtn.io/status str version"""
 
-from urllib import request
+import requests
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as fetch:
-        page = fetch.read()
-        page = page.decode('utf-8')
-        print("Body response:")
-        print("\t- type: " + str(type(page)))
-        print("\t- content: " + str(page))
+    fetch = requests.get('https://intranet.hbtn.io/status')
+    page = fetch.text
+    print("Body response:")
+    print("\t- type: " + str(type(page)))
+    print("\t- content: " + str(page))
