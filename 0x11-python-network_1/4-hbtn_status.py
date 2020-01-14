fc17d857886bcf0feeb068acb145b944af669f0b
diegovivas <dieguitovivas@hotmail.com>
2020-01-14 17:36:07 +0000

    [feature] task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetches https://intranet.hbtn.io/status """
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as fetch:
+        page = fetch.read()
+        page = page.decode('utf-8')
+        print("Body response:")
+        print("\t- type: " + str(type(page)))
+        print("\t- content: " + str(page))
