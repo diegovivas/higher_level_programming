4fad3827c5edfe7d84d6085efd32d3c64ecfd425
diegovivas <dieguitovivas@hotmail.com>
2020-01-15 23:15:48 +0000

    [feature] task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fetches github api """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    palabra = 'https://api.github.com/user'
+    fetch = requests.get(palabra , auth=(sys.argv[1],sys.argv[2]))
+    page = fetch.json()
+    if 'id' in page:
+        print(page['id'])
+    else:
+        print('None')
