7eee605b53bf0e2ebc2644171088e8bbb7cad0c2
diegovivas <dieguitovivas@hotmail.com>
2020-01-15 23:17:37 +0000

    [fix] task 10 pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     palabra = 'https://api.github.com/user'
-    fetch = requests.get(palabra , auth=(sys.argv[1],sys.argv[2]))
+    fetch = requests.get(palabra, auth=(sys.argv[1], sys.argv[2]))
     page = fetch.json()
     if 'id' in page:
         print(page['id'])
