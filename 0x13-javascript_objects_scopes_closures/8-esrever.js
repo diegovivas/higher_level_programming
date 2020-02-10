fd02aa5a7510a5c8a2005267312854c0b4faa6d1
diegovivas <dieguitovivas@hotmail.com>
2020-02-10 18:59:49 +0000

    [fix] index 0 task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const relist = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     relist.push(list[i]);
   }
   return relist;
