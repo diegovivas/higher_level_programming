20fe75104689fc597f7274a676d4acc9d4c7d79d
diegovivas <dieguitovivas@hotmail.com>
2020-02-10 18:44:37 +0000

    [feature] task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  var count = 0;
-  for (var i = 0; i < list.length; ++i) {
+  let count = 0;
+  for (i = 0; i < list.length; ++i) {
     if (list[i] === searchElement) { count++; }
   }
   return count;
