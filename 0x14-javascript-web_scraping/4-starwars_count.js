625f6be486327609fc7e8860baa0d40de2d67060
diegovivas <dieguitovivas@hotmail.com>
2020-02-11 17:01:13 +0000

    [feature] task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv.slice(2)[0];
+request.get(id, function (err, response, body) {
+  if (err) {
+  } else {
+    const listMovies = JSON.parse(body).results;
+    let cont = 0;
+    for (let i = 0; i < listMovies.length; i++) {
+      const xhar = listMovies[i].characters;
+      for (let x = 0; x < xhar.length; x++) {
+        if (xhar[x] === 'https://swapi.co/api/people/18/') {
+          cont++;
+        }
+      }
+    }
+    console.log(cont);
+  }
+});
